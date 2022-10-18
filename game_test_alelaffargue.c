7eaeb7811104840377dc77a4058fe1491a41cf17
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2022-10-18 12:44:48 +0200

    add game_test_alelaffargue.c

---
+++ game_test_alelaffargue.c
@@ -0,0 +1,60 @@
+#define __USE_GNU
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+/*****TEST*****/
+bool test_testname(int argc, char *name)
+{
+    if(argc != 3){
+        return false;
+    }
+    printf("entréefonction\n");
+    if (strcmp("dummy", name) == 0)
+    {
+        printf("entrée\n");
+        return true;
+    }
+    return false;
+}
+
+/****USAGE****/
+void usage(int argc, char *argv[])
+{
+  fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
+  exit(EXIT_FAILURE);
+}
+
+/****MAIN****/
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        usage(argc, argv);
+    }
+
+    fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
+    bool ok = false;
+
+    if (strcmp("testname", argv[1]) == 0){
+      ok = test_testname(argc, argv[2]);
+    }
+
+    else
+    {
+        fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    // print test result
+    if (ok){
+        fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+}
