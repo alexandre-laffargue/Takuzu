02e79d8837efa4fb3b1cb03f00a8c40a5f0fa6bb
Anas Bahraoui <anas.bahraoui@etu.u-bordeaux.fr>
2022-10-18 12:38:05 +0200

    add game_test

---
+++ game_test_abahraoui.c
@@ -0,0 +1,50 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+
+void usage(int argc, char *argv[])
+{
+  fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
+  exit(EXIT_FAILURE);
+}
+
+
+bool test_dummy(){
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc == 1)
+    usage(argc, argv);
+
+  // start test
+  fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
+  bool ok = false;
+  if (strcmp("dummy", argv[1]) == 0)
+    ok = test_dummy();
+
+    else{
+    fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
+    exit(EXIT_FAILURE);
+
+  }
+
+  if(ok)
+  {
+    fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
+    return EXIT_SUCCESS;
+  }
+  else
+  {
+    fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+}
+
