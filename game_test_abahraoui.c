2aeefdd255b642dc83bd709398ee9c4db22a520c
Anas Bahraoui <anas.bahraoui@etu.u-bordeaux.fr>
2022-11-12 19:09:14 -0500

    update 10258

--- game_test_abahraoui.c
+++ game_test_abahraoui.c
@@ -54,7 +54,7 @@ bool test_game_play_move(){
   game_play_move(g,0,0,S_ONE);
   game_play_move(g,1,1,S_ONE);
   game_play_move(g,3,3,S_EMPTY);
-  game_play_move(g2,2,0,S_ONE);
+  game_play_move(g2,0,0,S_ONE);
   //game_play_move(g,1,2,S_EMPTY);
   //game_play_move(g,1,3,S_EMPTY);
 
@@ -62,7 +62,7 @@ bool test_game_play_move(){
   if(game_get_square(g,0,0)!=S_ONE){game_delete(g2);game_delete(g);return false;} 
   else if(game_get_square(g,1,1)!=S_ONE){game_delete(g2);game_delete(g);return false;} 
   else if(game_get_square(g,3,3)!=S_EMPTY){game_delete(g2);game_delete(g);return false;}
-  else if(game_get_square(g2,2,0)!=S_ONE){game_delete(g2);game_delete(g);return false;}
+  else if(game_get_square(g2,0,0)!=S_ONE){game_delete(g2);game_delete(g);return false;}
   //else if(game_get_square(g,1,2)!=S_IMMUTABLE_ONE){game_delete(g);return false;} 
   //else if(game_get_square(g,1,3)!=S_IMMUTABLE_ZERO){game_delete(g);return false;}
   game_delete(g);
