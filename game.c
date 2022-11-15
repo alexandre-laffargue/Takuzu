7c4fad5cb0b5df5af39d56c40fd094c5d31471b1
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2022-11-15 12:27:36 +0100

    Update game.c game_aux.c et .clang-format

--- game.c
+++ game.c
@@ -1,80 +1,54 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "game.h"
+
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include"game.h" 
+
 #include "game_aux.h"
 
-struct game_s{
-    square *square_array;
-    int size;
+struct game_s {
+  square* square_array;
+  int size;
 };
 
-game game_new(square* squares){
-    return NULL;
+game game_new(square* squares) { return NULL; }
+
+game game_new_empty(void) { return NULL; }
+
+game game_copy(cgame g) { return NULL; }
+
+bool game_equal(cgame g1, cgame g2) { return NULL; }
+
+void game_delete(game g) {}
+
+void game_set_square(game g, uint i, uint j, square s) {}
+
+square game_get_square(cgame g, uint i, uint j) {
+  square a = S_ZERO;
+  return a;
 }
 
-game game_new_empty(void){
-    return NULL;
+int game_get_number(cgame g, uint i, uint j) { return -1; }
+
+int game_get_next_square(cgame g, uint i, uint j, direction dir, uint dist) {
+  return -1;
 }
 
-game game_copy(cgame g){
-    return NULL;
+int game_get_next_number(cgame g, uint i, uint j, direction dir, uint dist) {
+  return -1;
 }
 
-bool game_equal(cgame g1, cgame g2){
-    return NULL;
-}
+bool game_is_empty(cgame g, uint i, uint j) { return NULL; }
 
-void game_delete(game g){
+bool game_is_immutable(cgame g, uint i, uint j) { return NULL; }
 
-}
+int game_has_error(cgame g, uint i, uint j) { return -1; }
 
-void game_set_square(game g, uint i, uint j, square s){
+bool game_check_move(cgame g, uint i, uint j, square s) { return NULL; }
 
-}
+void game_play_move(game g, uint i, uint j, square s) {}
 
-square game_get_square(cgame g, uint i, uint j){
-    square a = S_ZERO;
-    return a;
-}
+bool game_is_over(cgame g) { return NULL; }
 
-int game_get_number(cgame g, uint i, uint j){
-    return -1;
-}
-
-int game_get_next_square(cgame g, uint i, uint j, direction dir, uint dist){
-    return -1;
-}
-
-int game_get_next_number(cgame g, uint i, uint j, direction dir, uint dist){
-    return -1;
-}
-
-bool game_is_empty(cgame g, uint i, uint j){
-    return NULL;
-}
-
-bool game_is_immutable(cgame g, uint i, uint j){
-    return NULL;
-}
-
-int game_has_error(cgame g, uint i, uint j){
-    return -1;
-}
-
-bool game_check_move(cgame g, uint i, uint j, square s){
-    return NULL;
-}
-
-void game_play_move(game g, uint i, uint j, square s){
-    
-}
-
-bool game_is_over(cgame g){
-    return NULL;
-}
-
-void game_restart(game g){
-    
-}
+void game_restart(game g) {}
---
+++ game_aux.c
@@ -0,0 +1,20 @@
+#include "game_aux.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "game.h"
+
+void game_print(cgame g) {}
+
+game game_default(void) {
+  game a;
+  return a;
+}
+
+game game_default_solution(void) {
+  game a;
+  return a;
+}
