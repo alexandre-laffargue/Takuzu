d47c298e08c99e545dad219ab2e4e70fd12ed86d
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2022-11-19 00:27:26 +0100

    Update game.c, ajout d'une fonction pour allouer dynamiquement la mémoire , game_set_square() , game_get_square() , game_get_number()

--- game.c
+++ game.c
@@ -12,6 +12,20 @@ struct game_s {
   int size;
 };
 
+void *memory_alloc(uint size)
+{
+    void *tmp = malloc(size);
+    if (tmp == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed \n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        return tmp;
+    }
+}
+
 game game_new(square* squares) { return NULL; }
 
 game game_new_empty(void) { return NULL; }
@@ -22,13 +36,28 @@ bool game_equal(cgame g1, cgame g2) { return NULL; }
 
 void game_delete(game g) {}
 
-void game_set_square(game g, uint i, uint j, square s) {}
-
-square game_get_square(cgame g, uint i, uint j) {
-  return S_EMPTY;
+void game_set_square(game g, uint i, uint j, square s) {
+  uint index = (i*g->size) +j;
+  g->square_array[index] = s;
 }
 
-int game_get_number(cgame g, uint i, uint j) { return -1; }
+square game_get_square(cgame g, uint i, uint j) {
+  uint index = (i*g->size) +j;
+  return g->square_array[index];
+}
+
+int game_get_number(cgame g, uint i, uint j) { 
+  uint index = (i*g->size) +j;
+  square sq = g->square_array[index];
+  if(sq == S_ZERO || sq == S_IMMUTABLE_ZERO){
+    return 0;
+  }
+  else if(sq == S_ONE || sq == S_IMMUTABLE_ONE){
+    return 1;
+  }
+  else
+    return -1; 
+}
 
 int game_get_next_square(cgame g, uint i, uint j, direction dir, uint dist) {
   return -1;
