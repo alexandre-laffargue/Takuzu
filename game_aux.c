5b8a6d1bdc31a6336817d66990edac41b4483525
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2022-11-19 14:46:17 +0100

    Update game_aux.c, ajout de game_default_solution()

--- game_aux.c
+++ game_aux.c
@@ -14,5 +14,39 @@ game game_default(void) {
 }
 
 game game_default_solution(void) {
-  return NULL;
+  game g = game_default();
+  game_set_square(g, 0, 0, S_ZERO);
+  game_set_square(g, 0, 3, S_ONE);
+  game_set_square(g, 0, 4, S_ZERO);
+  game_set_square(g, 0, 5, S_ONE);
+
+  game_set_square(g, 1, 0, S_ZERO);
+  game_set_square(g, 1, 1, S_ONE);
+  game_set_square(g, 1, 2, S_ONE);
+  game_set_square(g, 1, 3, S_ZERO);
+  game_set_square(g, 1, 4, S_ONE);
+  game_set_square(g, 1, 5, S_ZERO);
+
+  game_set_square(g, 2, 0, S_ONE);
+  game_set_square(g, 2, 2, S_ZERO);
+  game_set_square(g, 2, 3, S_ONE);
+  game_set_square(g, 2, 5, S_ONE);
+
+  game_set_square(g, 3, 0, S_ONE);
+  game_set_square(g, 3, 3, S_ZERO);
+  game_set_square(g, 3, 4, S_ZERO);
+  game_set_square(g, 3, 5, S_ONE);
+
+  game_set_square(g, 4, 0, S_ZERO);
+  game_set_square(g, 4, 1, S_ONE);
+  game_set_square(g, 4, 3, S_ZERO);
+  game_set_square(g, 4, 4, S_ONE);
+
+  game_set_square(g, 5, 0, S_ONE);
+  game_set_square(g, 5, 1, S_ZERO);
+  game_set_square(g, 5, 2, S_ZERO);
+  game_set_square(g, 5, 3, S_ONE);
+  game_set_square(g, 5, 4, S_ONE);
+
+  return g;
 }
