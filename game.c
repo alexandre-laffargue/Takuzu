81e1f49253236a456d6dc0dfc8668db1deaf47d7
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2022-11-19 14:24:09 +0100

    Update game.c, ajout de game_get_next_square(), game_get_next_number() + ajout de test dans game_set_square(), game_get_square(), game_get_number()

--- game.c
+++ game.c
@@ -37,16 +37,25 @@ bool game_equal(cgame g1, cgame g2) { return NULL; }
 void game_delete(game g) {}
 
 void game_set_square(game g, uint i, uint j, square s) {
+  if(g == NULL || i >= DEFAULT_SIZE || j >= DEFAULT_SIZE || (s != S_EMPTY && s!= S_IMMUTABLE_ONE && s!= S_IMMUTABLE_ZERO && s != S_ONE && s != S_ZERO)){
+    exit(EXIT_FAILURE);
+  }
   uint index = (i*g->size) +j;
   g->square_array[index] = s;
 }
 
 square game_get_square(cgame g, uint i, uint j) {
+  if(g == NULL || i >= DEFAULT_SIZE || j >= DEFAULT_SIZE){
+    exit(EXIT_FAILURE);
+  }
   uint index = (i*g->size) +j;
   return g->square_array[index];
 }
 
-int game_get_number(cgame g, uint i, uint j) { 
+int game_get_number(cgame g, uint i, uint j) {
+  if(g == NULL || i >= DEFAULT_SIZE || j >= DEFAULT_SIZE){
+    exit(EXIT_FAILURE);
+  }
   uint index = (i*g->size) +j;
   square sq = g->square_array[index];
   if(sq == S_ZERO || sq == S_IMMUTABLE_ZERO){
@@ -60,11 +69,61 @@ int game_get_number(cgame g, uint i, uint j) {
 }
 
 int game_get_next_square(cgame g, uint i, uint j, direction dir, uint dist) {
-  return -1;
+  if(g == NULL || i >= DEFAULT_SIZE || j >= DEFAULT_SIZE || dist > 2){
+    exit(EXIT_FAILURE);
+  }
+  uint index = (i*g->size) +j;
+  if(dir == UP){
+    int coord = i - dist;
+    if(coord >= 0 && coord < g->size){
+      uint indexf = index - (dist*g->size);
+      square sq = g->square_array[indexf];
+      return sq;
+    }else{return -1;}
+  }
+  else if (dir == DOWN){
+    int coord = i + dist;
+    if(coord >= 0 && coord < g->size){
+      uint indexf = index + (dist*g->size);
+      square sq = g->square_array[indexf];
+      return sq;
+    }else{return -1;}
+  }
+  else if (dir == LEFT){
+    int coord = j - dist;
+    if(coord >= 0 && coord < g->size){
+      uint indexf = index - dist;
+      square sq = g->square_array[indexf];
+      return sq;
+    }else{return -1;}
+  }
+  else if (dir == RIGHT){
+    int coord = j + dist;
+    if(coord >= 0 && coord < g->size){
+      uint indexf = index + dist;
+      square sq = g->square_array[indexf];
+      return sq;
+    }else{return -1;}
+  }
+  exit(EXIT_FAILURE);
 }
 
 int game_get_next_number(cgame g, uint i, uint j, direction dir, uint dist) {
-  return -1;
+  if(g == NULL || i >= DEFAULT_SIZE || j >= DEFAULT_SIZE || dist > 2){
+    exit(EXIT_FAILURE);
+  }
+  int sq = game_get_next_square(g, i, j, dir, dist);
+  if(sq == -1){
+    return -1;
+  }
+  if(sq == S_ZERO || sq == S_IMMUTABLE_ZERO){
+    return 0;
+  }
+  else if(sq == S_ONE || sq == S_IMMUTABLE_ONE){
+    return 1;
+  }
+  else
+    return -1; 
 }
 
 bool game_is_empty(cgame g, uint i, uint j) { return NULL; }
