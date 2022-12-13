13a7b2b6ddeab75c5ebdd79ed60179b64e41438d
Romain Lalanne <romain.lalanne4@etu.u-bordeaux.fr>
2022-12-13 11:56:17 +0100

    ajout de game_new_ext et game_new_empty_et

--- game.c
+++ game.c
@@ -355,3 +355,40 @@ uint game_nb_rows(cgame g){
   }
   return g->nb_rows; 
 }
+
+
+
+game game_new_empty_ext(uint nb_rows, uint nb_cols, bool wrapping, bool unique){
+  game g = memory_alloc(sizeof(struct game_s));
+  g->nb_rows = nb_rows;
+  g->nb_cols = nb_cols;
+  g->wrapping = wrapping;
+  g->unique = unique;
+  square* array = memory_alloc((g->nb_rows * g->nb_cols) * sizeof(square));
+
+  for (int i = 0; i < g->nb_rows * g->nb_cols; i++) {
+    array[i] = S_EMPTY;
+  }
+  g->square_array = array;
+  return g;
+
+}
+
+game game_new_ext(uint nb_rows, uint nb_cols, square* squares, bool wrapping, bool unique){
+  if (squares == NULL) {
+    exit(EXIT_FAILURE);
+  }
+  game new = memory_alloc(sizeof(struct game_s));
+  new->nb_rows = nb_rows;
+  new->nb_cols = nb_cols;
+  new->wrapping = wrapping;
+  new->unique = unique;
+  square* array = memory_alloc((new->nb_rows* new->nb_cols) * sizeof(square));
+
+  for (int i = 0; i < (new->nb_rows* new->nb_cols); i++) {
+    square tmp = squares[i];
+    array[i] = tmp;
+  }
+  new->square_array = array;
+  return new;
+}
