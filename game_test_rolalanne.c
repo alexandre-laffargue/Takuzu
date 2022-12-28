a3176add6155b6f61dec9d6cbfab789bf5aa63d7
Romain Lalanne <romain.lalanne.4@etu.u-bordeaux.fr>
2022-12-28 19:41:13 +0100

    Update game_test_rolalanne.c

--- game_test_rolalanne.c
+++ game_test_rolalanne.c
@@ -13,6 +13,8 @@ void usage(int argc, char *argv[]) {
 
 bool test_dummy() { return true; }
 
+bool test_game_is_wrapping() { return true; }
+
 bool test_game_print() {
   game g = game_default();
   game_print(g);
@@ -173,6 +175,9 @@ int main(int argc, char *argv[]) {
 
   } else if (strcmp("game_new_empty", argv[1]) == 0) {
     tmp = test_game_new_empty();
+
+  } else if (strcmp("game_is_wrapping", argv[1]) == 0) {
+    tmp = test_game_is_wrapping();
 
   } else if (strcmp("game_copy", argv[1]) == 0) {
     tmp = test_game_copy();
