bce5929ae977d0ea002b79e9e03152b63d3d9338
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2023-01-20 21:47:28 +0100

    Update game_test_alelaffargue.c, factorisation des if

--- game_test_alelaffargue.c
+++ game_test_alelaffargue.c
@@ -14,8 +14,8 @@ bool test_dummy() { return true; }
 bool test_game_set_square() {
   game g = game_default();
   game_set_square(g, 1, 0, S_ONE);
-  if (game_get_square(g, 1, 0) != S_ONE &&
-      game_get_square(g, 0, 1) != S_EMPTY) {
+  if (game_get_square(g, 1, 0) != S_ONE ||
+      game_get_square(g, 0, 1) != S_IMMUTABLE_ONE) {
     game_delete(g);
     return false;
   }
@@ -61,19 +61,10 @@ bool test_game_get_square() {
   square_array[3] = S_IMMUTABLE_ZERO;
   game g = game_new(square_array);
   free(square_array);
-  if (game_get_square(g, 0, 0) != S_ZERO) {
-    game_delete(g);
-    return false;
-  } else if (game_get_square(g, 0, 1) != S_ONE) {
-    game_delete(g);
-    return false;
-  } else if (game_get_square(g, 0, 2) != S_IMMUTABLE_ONE) {
-    game_delete(g);
-    return false;
-  } else if (game_get_square(g, 0, 3) != S_IMMUTABLE_ZERO) {
-    game_delete(g);
-    return false;
-  } else if (game_get_square(g, 0, 4) != S_EMPTY) {
+  if (game_get_square(g, 0, 0) != S_ZERO || game_get_square(g, 0, 1) != S_ONE ||
+      game_get_square(g, 0, 2) != S_IMMUTABLE_ONE ||
+      game_get_square(g, 0, 3) != S_IMMUTABLE_ZERO ||
+      game_get_square(g, 0, 4) != S_EMPTY) {
     game_delete(g);
     return false;
   } else {
@@ -94,19 +85,9 @@ bool test_game_get_number() {
   square_array[3] = S_IMMUTABLE_ZERO;
   game g = game_new(square_array);
   free(square_array);
-  if (game_get_number(g, 0, 0) != 0) {
-    game_delete(g);
-    return false;
-  } else if (game_get_number(g, 0, 1) != 1) {
-    game_delete(g);
-    return false;
-  } else if (game_get_number(g, 0, 2) != 1) {
-    game_delete(g);
-    return false;
-  } else if (game_get_number(g, 0, 3) != 0) {
-    game_delete(g);
-    return false;
-  } else if (game_get_number(g, 0, 4) != -1) {
+  if (game_get_number(g, 0, 0) != 0 || game_get_number(g, 0, 1) != 1 ||
+      game_get_number(g, 0, 2) != 1 || game_get_number(g, 0, 3) != 0 ||
+      game_get_number(g, 0, 4) != -1) {
     game_delete(g);
     return false;
   } else {
@@ -120,10 +101,6 @@ bool test_game_get_next_square() {
   for (int i = 0; i < 36; i++) {
     square_array[i] = S_EMPTY;
   }
-  square *square_array_with_wrapping = malloc(sizeof(square) * (4 * 6));
-  for (int i = 0; i < 24; i++) {
-    square_array_with_wrapping[i] = S_EMPTY;
-  }
   square_array[0] = S_ZERO;
   square_array[1] = S_ONE;
   square_array[2] = S_IMMUTABLE_ONE;
@@ -133,7 +110,38 @@ bool test_game_get_next_square() {
   square_array[17] = S_IMMUTABLE_ONE;
   square_array[32] = S_ONE;
   square_array[34] = S_ZERO;
+  game g = game_new(square_array);
+  free(square_array);
+  // test à l'interieur de la grille
+  if (game_get_next_square(g, 1, 0, UP, 1) != S_ZERO ||
+      game_get_next_square(g, 1, 0, DOWN, 1) != S_ONE ||
+      game_get_next_square(g, 0, 1, LEFT, 1) != S_ZERO ||
+      game_get_next_square(g, 0, 1, RIGHT, 1) != S_IMMUTABLE_ONE ||
+      game_get_next_square(g, 2, 0, UP, 2) != S_ZERO ||
+      game_get_next_square(g, 0, 0, DOWN, 2) != S_ONE ||
+      game_get_next_square(g, 0, 5, LEFT, 2) != S_IMMUTABLE_ZERO ||
+      game_get_next_square(g, 2, 0, RIGHT, 2) != S_EMPTY) {
+    game_delete(g);
+    return false;
+  }
+  // début test à travers les bordures avec l'option wrapping fausse.
+  else if (game_get_next_square(g, 0, 5, RIGHT, 1) != -1 ||
+           game_get_next_square(g, 0, 5, RIGHT, 2) != -1 ||
+           game_get_next_square(g, 1, 0, LEFT, 1) != -1 ||
+           game_get_next_square(g, 3, 1, LEFT, 2) != -1 ||
+           game_get_next_square(g, 0, 2, UP, 1) != -1 ||
+           game_get_next_square(g, 1, 4, UP, 2) != -1 ||
+           game_get_next_square(g, 5, 1, DOWN, 1) != -1 ||
+           game_get_next_square(g, 4, 2, DOWN, 2) != -1) {
+    game_delete(g);
+    return false;
+  }
+  game_delete(g);
 
+  square *square_array_with_wrapping = malloc(sizeof(square) * (4 * 6));
+  for (int i = 0; i < 24; i++) {
+    square_array_with_wrapping[i] = S_EMPTY;
+  }
   square_array_with_wrapping[3] = S_ONE;
   square_array_with_wrapping[7] = S_ONE;
   square_array_with_wrapping[8] = S_IMMUTABLE_ONE;
@@ -142,119 +150,25 @@ bool test_game_get_next_square() {
   square_array_with_wrapping[20] = S_ONE;
   square_array_with_wrapping[22] = S_ZERO;
   square_array_with_wrapping[23] = S_IMMUTABLE_ZERO;
-
-  game g = game_new(square_array);
   game g_with_wrapping =
       game_new_ext(4, 6, square_array_with_wrapping, true, true);
-  free(square_array);
   free(square_array_with_wrapping);
-  if (game_get_next_square(g, 1, 0, UP, 1) !=
-      S_ZERO) {  // test à l'interieur de la grille
-    game_delete(g);
+
+  // début test à travers les bordures
+  if (game_get_next_square(g_with_wrapping, 2, 5, RIGHT, 1) != S_ZERO ||
+      game_get_next_square(g_with_wrapping, 1, 5, RIGHT, 2) != S_ONE ||
+      game_get_next_square(g_with_wrapping, 3, 0, LEFT, 1) !=
+          S_IMMUTABLE_ZERO ||
+      game_get_next_square(g_with_wrapping, 2, 1, LEFT, 2) != S_IMMUTABLE_ONE ||
+      game_get_next_square(g_with_wrapping, 0, 2, UP, 1) != S_ONE ||
+      game_get_next_square(g_with_wrapping, 1, 4, UP, 2) != S_ZERO ||
+      game_get_next_square(g_with_wrapping, 3, 3, DOWN, 1) != S_ONE ||
+      game_get_next_square(g_with_wrapping, 3, 2, DOWN, 2) != S_IMMUTABLE_ONE) {
     game_delete(g_with_wrapping);
     return false;
-  } else if (game_get_next_square(g, 1, 0, DOWN, 1) != S_ONE) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 0, 1, LEFT, 1) != S_ZERO) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 0, 1, RIGHT, 1) != S_IMMUTABLE_ONE) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 2, 0, UP, 2) != S_ZERO) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 0, 0, DOWN, 2) != S_ONE) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 0, 5, LEFT, 2) != S_IMMUTABLE_ZERO) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 2, 0, RIGHT, 2) != S_EMPTY) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // début test à travers les bordures
-  else if (game_get_next_square(g_with_wrapping, 2, 5, RIGHT, 1) != S_ZERO) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g_with_wrapping, 1, 5, RIGHT, 2) != S_ONE) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g_with_wrapping, 3, 0, LEFT, 1) !=
-             S_IMMUTABLE_ZERO) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g_with_wrapping, 2, 1, LEFT, 2) !=
-             S_IMMUTABLE_ONE) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g_with_wrapping, 0, 2, UP, 1) != S_ONE) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g_with_wrapping, 1, 4, UP, 2) != S_ZERO) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g_with_wrapping, 3, 3, DOWN, 1) != S_ONE) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g_with_wrapping, 3, 2, DOWN, 2) !=
-             S_IMMUTABLE_ONE) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // début test à travers les bordures avec l'option wrapping fausse.
-  else if (game_get_next_square(g, 0, 5, RIGHT, 1) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 0, 5, RIGHT, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 1, 0, LEFT, 1) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 3, 1, LEFT, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 0, 2, UP, 1) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 1, 4, UP, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 5, 1, DOWN, 1) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_square(g, 4, 2, DOWN, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return true;
   }
+  game_delete(g_with_wrapping);
+  return true;
 }
 
 bool test_game_get_next_number() {
@@ -262,10 +176,6 @@ bool test_game_get_next_number() {
   for (int i = 0; i < 36; i++) {
     square_array[i] = S_EMPTY;
   }
-  square *square_array_with_wrapping = malloc(sizeof(square) * (4 * 6));
-  for (int i = 0; i < 24; i++) {
-    square_array_with_wrapping[i] = S_EMPTY;
-  }
   square_array[0] = S_ZERO;
   square_array[1] = S_ONE;
   square_array[2] = S_IMMUTABLE_ONE;
@@ -275,7 +185,37 @@ bool test_game_get_next_number() {
   square_array[17] = S_IMMUTABLE_ONE;
   square_array[32] = S_ONE;
   square_array[34] = S_ZERO;
+  game g = game_new(square_array);
+  free(square_array);
+  if (game_get_next_number(g, 1, 0, UP, 1) != 0 ||
+      game_get_next_number(g, 1, 0, DOWN, 1) != 1 ||
+      game_get_next_number(g, 0, 1, LEFT, 1) != 0 ||
+      game_get_next_number(g, 0, 1, RIGHT, 1) != 1 ||
+      game_get_next_number(g, 2, 0, UP, 2) != 0 ||
+      game_get_next_number(g, 0, 0, DOWN, 2) != 1 ||
+      game_get_next_number(g, 0, 5, LEFT, 2) != 0 ||
+      game_get_next_number(g, 2, 0, RIGHT, 2) != -1) {
+    game_delete(g);
+    return false;
+  }
+  // début test à travers les bordures avec l'option wrapping fausse.
+  else if (game_get_next_number(g, 0, 5, RIGHT, 1) != -1 ||
+           game_get_next_number(g, 0, 5, RIGHT, 2) != -1 ||
+           game_get_next_number(g, 1, 0, LEFT, 1) != -1 ||
+           game_get_next_number(g, 3, 1, LEFT, 2) != -1 ||
+           game_get_next_number(g, 0, 2, UP, 1) != -1 ||
+           game_get_next_number(g, 1, 4, UP, 2) != -1 ||
+           game_get_next_number(g, 5, 1, DOWN, 1) != -1 ||
+           game_get_next_number(g, 4, 2, DOWN, 2) != -1) {
+    game_delete(g);
+    return false;
+  }
+  game_delete(g);
 
+  square *square_array_with_wrapping = malloc(sizeof(square) * (4 * 6));
+  for (int i = 0; i < 24; i++) {
+    square_array_with_wrapping[i] = S_EMPTY;
+  }
   square_array_with_wrapping[3] = S_ONE;
   square_array_with_wrapping[7] = S_ONE;
   square_array_with_wrapping[8] = S_IMMUTABLE_ONE;
@@ -284,123 +224,23 @@ bool test_game_get_next_number() {
   square_array_with_wrapping[20] = S_ONE;
   square_array_with_wrapping[22] = S_ZERO;
   square_array_with_wrapping[23] = S_IMMUTABLE_ZERO;
-
-  game g = game_new(square_array);
   game g_with_wrapping =
       game_new_ext(4, 6, square_array_with_wrapping, true, true);
-  free(square_array);
   free(square_array_with_wrapping);
-  if (game_get_next_number(g, 1, 0, UP, 1) != 0) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // case valant S_ZERO
-  else if (game_get_next_number(g, 1, 0, DOWN, 1) != 1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // case valant S_ONE
-  else if (game_get_next_number(g, 0, 1, LEFT, 1) != 0) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // case valant S_ZERO
-  else if (game_get_next_number(g, 0, 1, RIGHT, 1) != 1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // case valant S_IMMUTABLE_ONE
-  else if (game_get_next_number(g, 2, 0, UP, 2) != 0) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // case valant S_ZERO
-  else if (game_get_next_number(g, 0, 0, DOWN, 2) != 1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // case valant S_ONE
-  else if (game_get_next_number(g, 0, 5, LEFT, 2) != 0) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // case valant S_IMMUTABLE_ZERO
-  else if (game_get_next_number(g, 2, 0, RIGHT, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // case valant S_EMPTY
   // début test à travers les bordures
-  else if (game_get_next_number(g_with_wrapping, 2, 5, RIGHT, 1) != 0) {
-    game_delete(g);
+  if (game_get_next_number(g_with_wrapping, 2, 5, RIGHT, 1) != 0 ||
+      game_get_next_number(g_with_wrapping, 1, 5, RIGHT, 2) != 1 ||
+      game_get_next_number(g_with_wrapping, 3, 0, LEFT, 1) != 0 ||
+      game_get_next_number(g_with_wrapping, 2, 1, LEFT, 2) != 1 ||
+      game_get_next_number(g_with_wrapping, 0, 2, UP, 1) != 1 ||
+      game_get_next_number(g_with_wrapping, 1, 4, UP, 2) != 0 ||
+      game_get_next_number(g_with_wrapping, 3, 3, DOWN, 1) != 1 ||
+      game_get_next_number(g_with_wrapping, 3, 2, DOWN, 2) != 1) {
     game_delete(g_with_wrapping);
     return false;
-  } else if (game_get_next_number(g_with_wrapping, 1, 5, RIGHT, 2) != 1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g_with_wrapping, 3, 0, LEFT, 1) != 0) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g_with_wrapping, 2, 1, LEFT, 2) != 1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g_with_wrapping, 0, 2, UP, 1) != 1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g_with_wrapping, 1, 4, UP, 2) != 0) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g_with_wrapping, 3, 3, DOWN, 1) != 1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g_with_wrapping, 3, 2, DOWN, 2) != 1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  }  // début test à travers les bordures avec l'option wrapping fausse.
-  else if (game_get_next_number(g, 0, 5, RIGHT, 1) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g, 0, 5, RIGHT, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g, 1, 0, LEFT, 1) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g, 3, 1, LEFT, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g, 0, 2, UP, 1) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g, 1, 4, UP, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g, 5, 1, DOWN, 1) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else if (game_get_next_number(g, 4, 2, DOWN, 2) != -1) {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return false;
-  } else {
-    game_delete(g);
-    game_delete(g_with_wrapping);
-    return true;
   }
+  game_delete(g_with_wrapping);
+  return true;
 }
 
 bool test_game_is_immutable() {
@@ -416,34 +256,14 @@ bool test_game_is_immutable() {
   square_array[6] = S_IMMUTABLE_ONE;
   game g = game_new(square_array);
   free(square_array);
-  if (!game_is_immutable(g, 0, 2)) {
+  if (!game_is_immutable(g, 0, 2) || !game_is_immutable(g, 0, 3) ||
+      !game_is_immutable(g, 1, 0) || game_is_immutable(g, 0, 0) ||
+      game_is_immutable(g, 0, 1) || game_is_immutable(g, 0, 4)) {
     game_delete(g);
     return false;
-  }  // case valant S_IMMUTABLE_ONE
-  else if (!game_is_immutable(g, 0, 3)) {
-    game_delete(g);
-    return false;
-  }  // case valant S_IMMUTABLE_ZERO
-  else if (!game_is_immutable(g, 1, 0)) {
-    game_delete(g);
-    return false;
-  }  // case valant S_IMMUTABLE_ONE
-  else if (game_is_immutable(g, 0, 0)) {
-    game_delete(g);
-    return false;
-  }  // case valant S_ZERO
-  else if (game_is_immutable(g, 0, 1)) {
-    game_delete(g);
-    return false;
-  }  // case valant S_ONE
-  else if (game_is_immutable(g, 0, 4)) {
-    game_delete(g);
-    return false;
-  }  // case valant S_EMPTY
-  else {
-    game_delete(g);
-    return true;
   }
+  game_delete(g);
+  return true;
 }
 
 bool test_game_default_solution() {
