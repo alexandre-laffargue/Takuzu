8d9cf8b465f77d77745719c85467e5efbf121101
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2023-01-20 22:43:05 +0100

    Update test_game_play_move, rectification pour le bug 11

--- game_test_abahraoui.c
+++ game_test_abahraoui.c
@@ -41,16 +41,15 @@ bool test_game_play_move() {
   game_play_move(g, 0, 0, S_ONE);
   game_play_move(g, 1, 1, S_ZERO);
   game_play_move(g, 3, 3, S_EMPTY);
-  game_play_move(g, 1, 2, S_ONE);
-  game_play_move(g, 1, 3, S_ZERO);
+  game_play_move(g, 2, 1, S_ONE);
 
   if (game_get_square(g, 0, 0) != S_ONE || game_get_square(g, 1, 1) != S_ZERO ||
       game_get_square(g, 3, 3) != S_EMPTY ||
-      game_get_square(g, 1, 2) != S_IMMUTABLE_ONE ||
-      game_get_square(g, 1, 3) != S_IMMUTABLE_ZERO) {
+      game_get_square(g, 2, 1) != S_ONE) {
     game_delete(g);
     return false;
   }
+  game_undo(g);
   game_undo(g);
   if (game_get_square(g, 3, 3) != S_ONE) {
     game_delete(g);
