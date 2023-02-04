35f1ce60101a15852f205b6737a6a1de9d125cfb
Romain Lalanne <romain.lalanne.4@etu.u-bordeaux.fr>
2023-02-04 15:30:34 +0100

    ajout de la fct game_save

--- game_tools.c
+++ game_tools.c
@@ -1,11 +1,11 @@
+#include "game_tools.h"
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "game_tools.h"
-#include "game_struct.h"
 #include "game_ext.h"
-
+#include "game_struct.h"
 
 game game_load(char *filename) {
   FILE *f = fopen(filename, "r");
@@ -39,4 +39,37 @@ game game_load(char *filename) {
   return g;
 }
 
-void game_save(cgame g, char *filename);
+void game_save(cgame g, char *filename) {
+  FILE *f = fopen(filename, "w");
+  if (f == NULL) {
+    printf("Error: cannot open file %s\n", filename);
+    exit(EXIT_FAILURE);
+  }
+  char *c;
+  fprintf(f, "%d %d %d %d\n", game_nb_rows(g), game_nb_cols(g),
+          game_is_wrapping(g), game_is_unique(g));
+  for (int i = 0; i < game_nb_rows(g); i++) {
+    for (int j = 0; j < game_nb_cols(g); j++) {
+      square sq = game_get_square(g, i, j);
+      if (sq == S_EMPTY) {
+        c = "e";
+      }
+      if (sq == S_IMMUTABLE_ONE) {
+        c = "B";
+      }
+      if (sq == S_IMMUTABLE_ZERO) {
+        c = "W";
+      }
+      if (sq == S_ONE) {
+        c = "b";
+      }
+      if (sq == S_ZERO) {
+        c = "w";
+      }
+
+      fprintf(f, "%s", c);
+    }
+    fprintf(f, "\n");
+  }
+  fclose(f);
+}
