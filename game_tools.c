5751d6943e12f159cce52c35902b530b694235dd
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2023-02-28 14:04:54 +0100

    Update game_tools.c, add game_solve() with game_build_solution(), add game_nb_solutions() with game_build_nb()

--- game_tools.c
+++ game_tools.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "game_aux.h"
 #include "game_ext.h"
 #include "game_struct.h"
 
@@ -73,4 +74,131 @@ void game_save(cgame g, char *filename) {
     fprintf(f, "\n");
   }
   fclose(f);
+}
+
+bool game_build_solution(game g, int i, int j) {
+  if (game_is_over(g)) {
+    game_print(g);
+    return true;
+  }
+  bool retour1;
+  bool retour2;
+
+  if (game_get_square(g, i, j) == S_IMMUTABLE_ONE ||
+      game_get_square(g, i, j) == S_IMMUTABLE_ZERO) {
+    if ((uint)(j + 1) < game_nb_cols(g)) {
+      j++;
+    } else if (((uint)(j + 1) == game_nb_cols(g)) &&
+               ((uint)(i + 1) < game_nb_rows(g))) {
+      i++;
+      j = 0;
+    }
+  }
+
+  if ((uint)(j + 1) < game_nb_cols(g)) {
+    game_set_square(g, i, j, S_ONE);
+    retour1 = game_build_solution(g, i, j + 1);
+    if (!retour1) {
+      game_set_square(g, i, j, S_ZERO);
+      retour2 = game_build_solution(g, i, j + 1);
+    }
+  } else if (((uint)(j + 1) == game_nb_cols(g)) &&
+             ((uint)(i + 1) < game_nb_rows(g))) {
+    game_set_square(g, i, j, S_ONE);
+    retour1 = game_build_solution(g, i + 1, 0);
+    if (!retour1) {
+      game_set_square(g, i, j, S_ZERO);
+      retour2 = game_build_solution(g, i + 1, 0);
+    }
+  } else if (((uint)(j + 1) == game_nb_cols(g)) &&
+             ((uint)(i + 1) == game_nb_rows(g))) {
+    game_set_square(g, i, j, S_ONE);
+    if (game_is_over(g)) {
+      retour1 = true;
+      game_print(g);
+    }
+    if (!retour1) {
+      game_set_square(g, i, j, S_ZERO);
+      if (game_is_over(g)) {
+        retour2 = true;
+        game_print(g);
+      }
+    }
+  } else {
+    return false;
+  }
+  return retour1 || retour2;
+}
+
+bool game_solve(game g) {
+  if (game_is_over(g)) return true;
+
+  int i = 0;
+  int j = 0;
+
+  bool ret = game_build_solution(g, i, j);
+  if (ret)
+    printf("Il y a une ou plusieurs solutions.\n");
+  else
+    printf("Il n'y a pas de solution.\n");
+  return ret;
+}
+
+uint game_build_nb(game g, int i, int j) {
+  if (game_is_over(g)) {
+    game_print(g);
+    return (uint)1;
+  }
+  uint rt1 = 0;
+  uint rt2 = 0;
+
+  if (game_get_square(g, i, j) == S_IMMUTABLE_ONE ||
+      game_get_square(g, i, j) == S_IMMUTABLE_ZERO) {
+    if ((uint)(j + 1) < game_nb_cols(g)) {
+      j++;
+    } else if (((uint)(j + 1) == game_nb_cols(g)) &&
+               ((uint)(i + 1) < game_nb_rows(g))) {
+      i++;
+      j = 0;
+    }
+  }
+
+  if ((uint)(j + 1) < game_nb_cols(g)) {
+    game_set_square(g, i, j, S_ONE);
+    rt1 = game_build_nb(g, i, j + 1);
+    game_set_square(g, i, j, S_ZERO);
+    rt2 = game_build_nb(g, i, j + 1);
+  } else if (((uint)(j + 1) == game_nb_cols(g)) &&
+             ((i + 1) < game_nb_rows(g))) {
+    game_set_square(g, i, j, S_ONE);
+    rt1 = game_build_nb(g, i + 1, 0);
+    game_set_square(g, i, j, S_ZERO);
+    rt2 = game_build_nb(g, i + 1, 0);
+  } else if (((uint)(j + 1) == game_nb_cols(g)) &&
+             ((i + 1) == game_nb_rows(g))) {
+    game_set_square(g, i, j, S_ONE);
+    if (game_is_over(g)) {
+      rt1 = (uint)1;
+      game_print(g);
+    }
+    game_set_square(g, i, j, S_ZERO);
+    if (game_is_over(g)) {
+      rt2 = (uint)1;
+      game_print(g);
+    }
+  } else {
+    return (uint)0;
+  }
+  return rt1 + rt2;
+}
+
+uint game_nb_solutions(cgame g) {
+  int i = 0;
+  int j = 0;
+  game gs = game_copy(g);
+
+  uint ret = game_build_nb(gs, i, j);
+  game_delete(gs);
+  printf("Il y a %d solution(s).\n", ret);
+  return ret;
 }
