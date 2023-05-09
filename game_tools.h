7321fa359b2786ee47b65f64d1eb2a1ee94da276
Romain Lalanne <romain.lalanne.4@etu.u-bordeaux.fr>
2023-05-09 15:07:44 +0200

    ajout de game_random

--- game_tools.h
+++ game_tools.h
@@ -1,6 +1,8 @@
 /**
  * @file game_tools.h
  * @brief Game Tools.
+ * @file game_random.h
+ * @brief Game Random.
  * @details See @ref index for further details.
  * @copyright University of Bordeaux. All rights reserved, 2022.
  *
@@ -8,6 +10,11 @@
 
 #ifndef __GAME_TOOLS_H__
 #define __GAME_TOOLS_H__
+#ifndef __GAME_RANDOM_H__
+#define __GAME_RANDOM_H__
+#include <stdbool.h>
+#include <stdio.h>
+
 
 #include "game.h"
 
@@ -54,3 +61,19 @@ uint game_nb_solutions(cgame g);
  */
 
 #endif // __GAME_TOOLS_H__
+
+/**
+ * Create a random game with a given size and number of walls
+ *
+ * @param nb_rows the number of rows of the game
+ * @param nb_cols the number of columns of the game
+ * @param wrapping wrapping option
+ * @param uniq unique option
+ * @param with_solution if true, the game contains the solution, otherwise only walls
+ *
+ * @return the generated random game
+ */
+game game_random(uint nb_rows, uint nb_cols, bool wrapping, uint uniq, bool with_solution);
+
+
+#endif  // __GAME_RANDOM_H__
