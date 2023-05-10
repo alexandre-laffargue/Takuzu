bb5110df1f092aa0c7c2049cc592cce96e805ed4
Alexandre Laffargue <alexandre.laffargue@etu.u-bordeaux.fr>
2023-05-10 15:37:06 +0000

    clang-format

--- web/game_script.js
+++ web/game_script.js
@@ -18,27 +18,27 @@ var btnredo = document.getElementById("redo");
 var btnrandom = document.getElementById("random");
 
 
-btnrestart.addEventListener("click", function() {
+btnrestart.addEventListener("click", function () {
     Module._restart(g);
     printGame(g);
 });
 
-btnsolve.addEventListener("click", function() {
+btnsolve.addEventListener("click", function () {
     Module._solve(g);
     printGame(g);
 });
 
-btnundo.addEventListener("click", function() {
+btnundo.addEventListener("click", function () {
     Module._undo(g);
     printGame(g);
 });
 
-btnredo.addEventListener("click", function() {
+btnredo.addEventListener("click", function () {
     Module._redo(g);
     printGame(g);
 });
 
-btnrandom.addEventListener("click", function() {
+btnrandom.addEventListener("click", function () {
     g = Module._new_random(6, 6, false, false);
     printGame(g);
 });
@@ -51,21 +51,21 @@ function canvasLeftClick(event) {
     var squareSize = canvas.width / Module._nb_cols(g);
     var row = Math.floor(y / squareSize);
     var col = Math.floor(x / squareSize);
-    var square = (Module._get_square(g, row, col) + 1) %3;
+    var square = (Module._get_square(g, row, col) + 1) % 3;
     Module._play_move(g, row, col, square);
     printGame(g);
 }
 
-window.addEventListener('resize', function(){
+window.addEventListener('resize', function () {
     resizeCanvas();
     printGame(g);
-}  ); 
+});
 
 function resizeCanvas() {
     const minSize = Math.min(window.innerWidth, window.innerHeight);
     canvas.width = minSize * 0.7;
     canvas.height = canvas.width;
-  }
+}
 
 function printGame(g) {
     var nb_rows = Module._nb_rows(g);
@@ -78,46 +78,39 @@ function printGame(g) {
             var immutable = Module._is_immutable(g, row, col);
             var empty = Module._is_empty(g, row, col);
             var error = Module._has_error(g, row, col);
-            if (empty)
-            {   
-                ctx.drawImage(emptycase,  col*squareSize, row*squareSize, squareSize, squareSize);
+            if (empty) {
+                ctx.drawImage(emptycase, col * squareSize, row * squareSize, squareSize, squareSize);
             }
 
-            else if (immutable && number == 0)
-            {
-                ctx.drawImage(kanjiBIM,  col*squareSize, row*squareSize, squareSize, squareSize);
-              }
-
-            else if (immutable && number == 1)
-            {
-                ctx.drawImage(kanjiNIM,  col*squareSize, row*squareSize, squareSize, squareSize);    
+            else if (immutable && number == 0) {
+                ctx.drawImage(kanjiBIM, col * squareSize, row * squareSize, squareSize, squareSize);
             }
 
-            else if (number == 0)
-            {
-                ctx.drawImage(kanjiB,  col*squareSize, row*squareSize, squareSize, squareSize);       
+            else if (immutable && number == 1) {
+                ctx.drawImage(kanjiNIM, col * squareSize, row * squareSize, squareSize, squareSize);
             }
 
-            else if (number == 1)
-            {
-                ctx.drawImage(kanjiN,  col*squareSize, row*squareSize, squareSize, squareSize);    
+            else if (number == 0) {
+                ctx.drawImage(kanjiB, col * squareSize, row * squareSize, squareSize, squareSize);
             }
 
-            else 
-            {
-                ctx.drawImage(emptycase,  col*squareSize, row*squareSize, squareSize, squareSize);
+            else if (number == 1) {
+                ctx.drawImage(kanjiN, col * squareSize, row * squareSize, squareSize, squareSize);
             }
 
-            if (error)
-            {
-                ctx.drawImage(errorcase,  col*squareSize, row*squareSize, squareSize, squareSize);
+            else {
+                ctx.drawImage(emptycase, col * squareSize, row * squareSize, squareSize, squareSize);
             }
-            
+
+            if (error) {
+                ctx.drawImage(errorcase, col * squareSize, row * squareSize, squareSize, squareSize);
+            }
+
         }
-        
+
     }
-    
-    
+
+
 }
 
 function start() {
