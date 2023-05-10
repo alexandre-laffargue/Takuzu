3e896e94aad4268d350850e2895dae7782d5418b
Romain Lalanne <romain.lalanne.4@etu.u-bordeaux.fr>
2023-05-10 13:34:26 +0200

    ajout de fct js

--- web/demo.js
+++ web/demo.js
@@ -39,3 +39,77 @@ function start() {
     Module._delete(g);
 }
 
+var canvas = document.getElementById('mycanvas');
+
+/* ******************** register events ******************** */
+
+window.addEventListener('load', windowLoad);              // window load
+canvas.addEventListener('click', canvasLeftClick);        // left click event
+canvas.addEventListener('contextmenu', canvasRightClick); // right click event
+
+/* ******************** event callback ******************** */
+
+function canvasLeftClick(event) {
+    event.preventDefault(); // prevent default context menu to appear...
+    // get relative cursor position in canvas
+    console.log("left click at position:", event.offsetX, event.offsetY);
+    // update position of mario image used by drawCanvas()
+    marioX = event.offsetX;
+    marioY = event.offsetY;
+    drawCanvas();
+}
+
+function canvasRightClick(event) {
+    event.preventDefault(); // prevent default context menu to appear...
+    // get relative cursor position in canvas
+    console.log("right click at position:", event.offsetX, event.offsetY);
+    // update position of mario image used by drawCanvas()
+    squareX = event.offsetX;
+    squareY = event.offsetY;
+    drawCanvas();
+}
+
+function windowLoad() {
+    console.log("window load");
+    drawCanvas();
+}
+
+/* ******************** canvas drawing ******************** */
+
+function drawCanvas() {
+    var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var height = canvas.height;
+
+    // clear canvas
+    ctx.clearRect(0, 0, width, height);
+
+    // draw a yellow square at (squareX, squareY) position
+    ctx.save();
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(squareX, squareY, 40, 40);
+    ctx.restore();
+
+    // draw some lines
+    ctx.save();
+    ctx.strokeStyle = 'blue';
+    ctx.moveTo(0, 0);
+    ctx.lineTo(width, height);
+    ctx.moveTo(width, 0);
+    ctx.lineTo(0, height);
+    ctx.stroke(); // the stroke will actually paint the current lines
+    ctx.restore();
+
+    // draw a centered text
+    ctx.save();
+    ctx.font = 'bold 20px Arial';
+    ctx.fillStyle = 'black';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+    ctx.fillText("Hello World!", width / 2, height / 2);
+    ctx.restore();
+
+    // draw mario image at (marioX, marioY) position
+    ctx.drawImage(mario, marioX, marioY, 80, 80);
+}
+
